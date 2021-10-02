4711ee609eb7e9601a1eabf226331b2238f61e63
Wemimo Ibidunmoye <53876543+wemiibidun@users.noreply.github.com>
2021-10-02 07:55:58 -0600

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,7 +16,7 @@ const Hero = (props) => (
         My name is Wemi & I'm a full stack web developer based in Canada. I build and design simple, responsive websites that your clients will love
       </SectionText>
 
-      <Button onClick={() => window.open("http://code.wemiibidun.com/")}>View My Resume</Button>
+      <Button onClick={() => window.open("https://drive.google.com/file/d/1MJ5JTlUG0eeyFUs0NXAxgW08AyrD8hbv/view?usp=sharing")}>View My Resume</Button>
     </LeftSection>
   </Section>
 );
